960c939f28cf0ed4acb65a4ea51c8f7152dc62b8
Robert Kelly <robert.kelly@3vuellc.com>
2024-04-02 10:17:36 -0500

    configure to add styles and packages

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,8 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { dts } from "rollup-plugin-dts";
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -14,17 +15,28 @@ export default [
                 format: "cjs",
                 sourcemap: true,
             },
-            { file: packageJson.module, format: "esm", sourcemap: true },
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true,
+            },
         ],
         plugins: [
             resolve(),
             commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
+            typescript({ tsconfig: "./tsconfig.json", sourceMap: false }),
+
+            // NEW
+            postcss(),
         ],
+        external: [...Object.keys(packageJson.peerDependencies || {})],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+
+        // NEW
+        external: [/\.css$/],
     },
 ];
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+// import "./Button.css";
+import { styled } from "@mui/material";
 
 interface ButtonProps {
     label: string;
 }
 
+const ButtonWrapper = styled("button")`
+    font-size: 5rem;
+    background-color: aquamarine;
+    border: none;
+`;
+
 const Button = (props: ButtonProps) => {
-    return <button>{props.label}</button>;
+    return <ButtonWrapper>{props.label}</ButtonWrapper>;
 };
 
 export default Button;
